8421e31755467f4e7671a1a29af2bb711085bf2b
grigorakakisgiorgos giorgos <grigorakakisgiorgos@gmail.com>
2017-03-30 18:45:09 +0300

    Create plan_matches.py

---
+++ plan_matches.py
@@ -0,0 +1,30 @@
+import sys
+file1 = open(sys.argv[1])
+lista = []
+lista1 = []
+for line in file1:
+    parts = line.split()
+    if parts[0]<parts[1]:
+        s1 = (str(parts[0]) , str(parts[1]),0)
+        lista.append(s1)
+    else:
+        s1 = (str(parts[1]) , str(parts[0]),0)
+        lista.append(s1)
+lista.sort()
+file1.close()
+file1 = open(sys.argv[1], 'w')
+for i in lista:
+    days=1
+    for x in lista1:
+        if days==x[2]:
+            if i[0]==x[0] or i[0]==x[1] or i[1]==x[1] or i[1]==x[0]:
+                days=days+1
+    m = (i[0],i[1],days)
+    lista1.append(m)
+    lista1.sort(key=lambda st: st[2])
+lista1.sort()
+for i in lista1:
+    s1 = '(' + str(i[0]) + ', ' + str(i[1]) + ') ' + str(i[2]) +'\n'
+    file1.write(s1)
+    print(s1)
+file1.close()
